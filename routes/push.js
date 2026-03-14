1017f29915ea2f97e7fca579318095ab31e7611f
makelabs04 <makelabsmail@gmail.com>
2026-03-14 12:35:26 +0530

    Update push.js

--- routes/push.js
+++ routes/push.js
@@ -9,10 +9,10 @@ const jwt      = require('jsonwebtoken');
 const webpush  = require('web-push');
 
 const DB = {
-  host:     'localhost',
-  user:     'root',
-  password: 'your_db_password',
-  database: 'rfid_attendance',
+  host:     '127.0.0.1',
+  user:     'u966260443_rfidNode',
+  password: 'Makelabs@123',
+  database: 'u966260443_rfidNode',
   waitForConnections: true,
   connectionLimit: 10
 };
